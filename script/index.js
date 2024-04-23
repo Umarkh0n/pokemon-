35bd2d56eb918edd493320ac0b37fbc061870263
Umarkhon <kudratkhujaev@bk.ru>
2024-04-24 00:46:16 +0500

    main and cart finish

--- script/index.js
+++ script/index.js
@@ -7,7 +7,17 @@ for (let item of pokemons) {
     
     const ListBox = document.createElement('li')
     const newImage = document.createElement('img')
+    const newSpan = document.createElement('span')
     const newDownCart = document.createElement('div')
+    const newDownCartWords = document.createElement('div')
+    const newDownCartName = document.createElement('h3') 
+    const newDownCartType = document.createElement('p')
+    const newDownCartBottom = document.createElement('div')
+    const newDownCartWeight = document.createElement('p')
+    const newDownCartHeight = document.createElement('p')
+    const newDownCartImg = document.createElement('div')
+    const newDownCartBtn = document.createElement('button')
+    const newDownCartLike = document.createElement('img')
      
     const newTitle = document.createElement('p')
     const cartList = document.createElement('ul')
@@ -16,29 +26,39 @@ for (let item of pokemons) {
 
 
 
-    ListBox.setAttribute('class', ' rounded-md')
+    ListBox.setAttribute('class', ' border-2 border-black bg-white rounded-xl')
     newImage.setAttribute('src', item.img)
     newImage.setAttribute('class', 'px-[75px] pt-7.5 pb-17')
+    newSpan.setAttribute('class', 'block w-full border border-black')
+    newDownCart.setAttribute('class', 'flex justify-between items-start px-7.5 pt-5 pb-[32px]  border-t-stone-950')
+    newDownCartBottom.setAttribute ('class', 'flex items-center gap-7.5')
+    newDownCartLike.setAttribute ('src', './image/cart-heart-logo.svg')
+    newDownCartLike.setAttribute ('class', ' hover:bg-[#FFD845] transition active:opacity-50 rounded-full p-1 ')
+    newDownCartName.setAttribute ('class', 'text-2xl font-bold')
+    newDownCartType.setAttribute ('class', 'text-xl font-semibold')
+    newDownCartWeight.setAttribute ('class', 'text-2xl font-bold')
+    newDownCartHeight.setAttribute ('class', 'text-2xl font-bold')
+    
 
     
-    newTitle.setAttribute('class', 'mb-4 text-white text-xl')
-    cartList.setAttribute('class', 'list-disc pl-8')
-    cartItem.setAttribute('class', 'text-white')
-    newBtnBookmark.setAttribute('class', 'bg-red-500 text-white px-3 py-1.5 rounded-md text-base mt-2 hover:bg-red-600 transition')
 
 
-
-
-    newTitle.textContent = item.title
-    cartItem.textContent = item.genres
-    newBtnBookmark.textContent = "Bookmark"
+    
+    newDownCartName.textContent = item.name
+    newDownCartType.textContent = item.type
+    newDownCartWeight.textContent =  item.weight 
+    newDownCartHeight.textContent = item.height
 
 
 
 
 
     elList.append(ListBox)
-    ListBox.append(newImage, newDownCart)
-    newDownCart.append(newTitle, cartList, newBtnBookmark)
-    cartList.append(cartItem)
+    ListBox.append(newImage, newSpan, newDownCart)
+    newDownCart.append(newDownCartWords, newDownCartImg )
+    newDownCartWords.append(newDownCartName, newDownCartType, newDownCartBottom)
+    newDownCartBottom.append(newDownCartWeight, newDownCartHeight)
+    newDownCartImg.append(newDownCartBtn)
+    newDownCartBtn.append(newDownCartLike)
+
 }
